479f30483b810e10701e2faa736b0cfb6292cc71
fyoumb <fyoumb@gmail.com>
2024-01-06 16:20:31 -0500

    vpc with default rt and default sg

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "my_ip" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -25,7 +26,7 @@ resource "aws_subnet" "myapp-subnet-1" {
 
 }
 
-
+/*
 resource "aws_route_table" "myapp-route-table" {
     vpc_id = aws_vpc.myapp-vpc.id
     route {
@@ -37,7 +38,7 @@ resource "aws_route_table" "myapp-route-table" {
     }
   
 }
-
+*/
 
 resource "aws_internet_gateway" "myapp-igw" {
     vpc_id = aws_vpc.myapp-vpc.id
@@ -47,15 +48,15 @@ resource "aws_internet_gateway" "myapp-igw" {
   
 }
 
-
+/*
 resource "aws_route_table_association" "a-rtb-subnet" {
     subnet_id = aws_subnet.myapp-subnet-1.id
     route_table_id = aws_route_table.myapp-route-table.id
   
 }
 
+*/
 
-/*
 resource "aws_default_route_table" "main-rtb" {
 default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id  
 
@@ -69,4 +70,80 @@ route {
     }
     
 }
+
+/*
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        #cidr_blocks = ["0.0.0.0/0", "192.168.1.1/32"]
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = [] # for endpoint access > Not relevant for this demo
+    
+    }
+
+    tags = {
+      
+      Name = "${var.env_prefix}-sg"
+    }
+  
+}
+
 */
+
+# Using a default security group
+
+
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        #cidr_blocks = ["0.0.0.0/0", "192.168.1.1/32"]
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = [] # for endpoint access > Not relevant for this demo
+    
+    }
+
+    tags = {
+      
+      Name = "${var.env_prefix}-default-sg"
+    }
+  
+}
+
+
